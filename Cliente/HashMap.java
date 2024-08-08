60f27d589c3148ac5cdce69af8a08f0ecef4ed77
RmFelipe24 <rimunozro@est.utn.ac.cr>
2024-08-08 16:55:37 -0600

    Se crea la clase HashMap
    Se crea la clase donde busca y agrega se hizo cambios

--- Cliente/HashMap.java
+++ Cliente/HashMap.java
@@ -8,6 +8,21 @@ package Cliente;
  *
  * @author munoz
  */
+import java.util.HashMap;
+
 public class HashMap {
     
+    HashMap<Cliente,Vehiculo> ClienteVehiculos;
+
+    public HashMap() {
+        ClienteVehiculos = new java.util.HashMap();
+    }
+       public void agregarVehiculo(Cliente cliente, Vehiculo vehiculo) {
+        ClienteVehiculos.put(cliente,vehiculo);
+    }
+        public String buscarPalabra(Cliente cliente, Vehiculo vehiculo) {
+        return ClienteVehiculos.getOrDefault(cliente,"Cliente no encontrado");
+    }
+        
+       
 }
